418dc6d252136fd30fd5baa0fb25e0ff2b67f8fe
Fadin <fadin.egor.v@gmail.com>
2021-02-28 21:42:15 +0300

    format doc

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-module.exports = function towelSort (matrix) {
-  let arr = new Array
-  if (!matrix) return arr
-  for (item of matrix) {
-    Number.isInteger((matrix.indexOf(item) + 1)/2) ? arr.push(item.reverse()) : arr.push(item)
-  } return arr.flat()
-}
+module.exports = function towelSort(matrix) {
+    let arr = new Array();
+    if (!matrix) return arr;
+    for (item of matrix) {
+        Number.isInteger((matrix.indexOf(item) + 1) / 2)
+            ? arr.push(item.reverse())
+            : arr.push(item);
+    }
+    return arr.flat();
+};
